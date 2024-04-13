1c5e6f4d6a0238db7208acb5f3c5390934933899
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-13 16:18:56 +0900

    add delete function

--- main.js
+++ main.js
@@ -16,33 +16,56 @@ btn.addEventListener('click', () => {
         date: current_str
     }
     listItems.push(item);
-    localStorage.store = JSON.stringify(listItems);
+    localStorage.setItem('data', JSON.stringify(listItems));
     // ページをリロード
     window.location.reload();
 })
 
 // ページをリロードするとローカルストレージの中身が再表示される
 document.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.store === undefined) {
+    const dataStr = localStorage.getItem('data');
+    if (dataStr === undefined) {
         return;
     }
-    listItems = JSON.parse(localStorage.store);
+    listItems = dataStr ? JSON.parse(dataStr) : [];
 
-    for (let item of listItems) {
+    listItems.forEach((item, index) => {
         // テーブルの部品生成
         const tr = document.createElement('tr');
+        tr.classList.add('align-middle');
+        const tdIndex = document.createElement('td');
         const td1 = document.createElement('td');
         const td2 = document.createElement('td');
         const td3 = document.createElement('td');
+        const td4 = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.onclick = () => deleteData(index);
+        deleteButton.classList.add('btn','btn-danger');
         // テーブルの部品にデータを挿入
+        tdIndex.innerText = index + 1;
         td1.innerText = item.name;
         td2.innerText = item.comment;
         td3.innerText = item.date;
+        td4.appendChild(deleteButton);
         // テーブルの部品を表示
+        tr.appendChild(tdIndex);
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
+        tr.appendChild(td4);
         tbody.appendChild(tr);
-    }
+    })
 })
+
+
+function deleteData(index) {
+    listItems.splice(index, 1);
+
+    // 配列をJSON形式に変換してlocalStorageに保存
+    localStorage.setItem('data', JSON.stringify(listItems));
+
+    // データリストを更新
+    window.location.reload();
+}
 
